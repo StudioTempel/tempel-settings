4fe059b0fcb57de2448fb8b5cabe69620a9234d8
Job Ligthart <job.ligthart2003@gmail.com>
2024-06-25 09:49:35 +0200

    Update version number

--- tempel.php
+++ tempel.php
@@ -11,7 +11,7 @@
  *
  * Plugin Name:       Tempel settings
  * Description:       Plugin that compliments custom-built themes produced by Studio Tempel
- * Version:           2.0.1
+ * Version:           2.0.2
  * Author:            Studio Tempel
  * Author URI:        https://studiotempel.nl
  * Text Domain:       tempel-settings
