f3f715ca7cfd08d6b893c5bfb6066f392d52a07e
Job Ligthart <job.ligthart2003@gmail.com>
2024-08-28 10:20:10 +0200

    Bump version to 2.1.3

--- tempel.php
+++ tempel.php
@@ -11,7 +11,7 @@
  *
  * Plugin Name:       Tempel settings
  * Description:       Plugin that compliments custom-built themes produced by Studio Tempel
- * Version:           2.1.2
+ * Version:           2.1.3
  * Author:            Studio Tempel
  * Author URI:        https://studiotempel.nl
  * Text Domain:       tempel-settings
